2ae02d0f54eb690534d14b2d0a28a4e83b9938fc
ঔৣ͜͡➳ 亇ቾ❍ ✟ア入れ刃入✟ <89053669+Thepan808@users.noreply.github.com>
2021-10-31 02:50:57 -0300

    Update playlist.py

--- VCBot/playlist.py
+++ VCBot/playlist.py
@@ -10,9 +10,9 @@ async def playlist(client, m: Message):
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"**🎧 NOW PLAYING:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", disable_web_page_preview=True)
+         await m.reply(f"**♦️ AGORA TOCANDO:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", disable_web_page_preview=True)
       else:
-         QUE = f"**🎧 NOW PLAYING:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
+         QUE = f"**♦️ AGORA TOCANDO:** \n[{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**⏯ PLAYLIST:**"
          l = len(chat_queue)
          for x in range (1, l):
             hmm = chat_queue[x][0]
@@ -21,4 +21,4 @@ async def playlist(client, m: Message):
             QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
          await m.reply(QUE, disable_web_page_preview=True)
    else:
-      await m.reply("`Nothing is Streaming`")
+      await m.reply("`😒 Não tem nada tocando porra, tem demência?`")
